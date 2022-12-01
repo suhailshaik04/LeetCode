29ac36390901fe8d883b39fb832006dea5af3f14
suhailshaik04 <98518088+suhailshaik04@users.noreply.github.com>
2022-12-01 17:41:32 +0530

    Time: 24 ms (77.23%), Space: 153.5 MB (33.54%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -9,51 +9,13 @@
  * }
  */
 class Solution {
-    public static ListNode addFrst(Stack<ListNode> st){
-        ListNode head = st.pop();
-        while(!st.isEmpty()){
-            ListNode curr = st.pop();
-            curr.next = head;
-            head = curr;
+    public ListNode removeNodes(ListNode head) {
+        if (head != null) {
+            head.next = removeNodes(head.next);
+            if (head.next != null && head.val < head.next.val) {
+                return head.next;
+            }
         }
         return head;
-    }
-    public ListNode removeNodes(ListNode head) {
-        /*
-        ListNode dummy = new ListNode(-1);
-        ListNode ptr = dummy;
-        ListNode curr = head;
-        boolean check = false;
-        while(curr.next!=null){
-            ListNode chk = curr.next;
-            while(chk!=null){
-                if(chk.val>curr.val){
-                    check=true;
-                    break;
-                }
-                chk = chk.next;
-            }
-            if(check==false){
-                    ptr.next = curr;
-                    ptr = ptr.next;
-            }
-            curr = curr.next;
-            check= false;
-        }
-        ptr.next = curr;
-        return dummy.next;
-        */
-        ListNode dummy = new ListNode(-1);
-        Stack<ListNode> st = new Stack<>();
-        ListNode curr = head;
-        while(curr!=null){
-            while(!st.isEmpty() && st.peek().val<curr.val){
-            st.pop();
-            }
-            st.push(curr);
-            curr = curr.next;
-        }
-        ListNode ans = addFrst(st);
-        return ans;
     }
 }
