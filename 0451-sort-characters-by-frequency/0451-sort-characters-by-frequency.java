5e67db7a49cc3ea7862eb9d14486270e291818d5
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2022-12-03 13:44:20 +0530

    Time: 43 ms (46.28%), Space: 49 MB (63.08%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -4,15 +4,18 @@ class Solution {
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }      
-       
-        List<Character> chars = new ArrayList(map.keySet());
-	    Collections.sort(chars, (a, b) -> (map.get(b) - map.get(a)));
-	    StringBuilder sb = new StringBuilder();
-	    for (Object c : chars) {
+        PriorityQueue <Character> pQ = new PriorityQueue((a, b) -> map.get(b) - map.get(a));
+        for (char c : map.keySet()) {
+            pQ.offer(c);
+        }
+        
+        StringBuilder sb = new StringBuilder();       
+        while (!pQ.isEmpty()) {
+            char c = pQ.poll();
             for (int i = 0; i < map.get(c); i++) {
                 sb.append(c);
             }
-        }
+        }    
         return sb.toString();
     }
 }
