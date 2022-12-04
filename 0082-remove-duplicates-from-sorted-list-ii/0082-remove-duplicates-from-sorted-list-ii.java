708acff8d3e0c298e4ea650aa8408b242b35f8e0
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2022-12-04 11:50:53 +0530

    Time: 1 ms (82.76%), Space: 43.2 MB (73.40%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        /*
         ListNode dummy = new ListNode(-1);
         ListNode ptr = dummy;
         Map<Integer,Integer> map = new LinkedHashMap<>();
@@ -29,6 +30,22 @@ class Solution {
                 ptr = ptr.next;
             }
         }
+        return dummy.next;
+        */
+        ListNode dummy = new ListNode(0,head);
+        ListNode ptr = dummy;
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                ptr.next = head.next;     
+            } 
+            else {
+                ptr = ptr.next;    
+            }
+            head = head.next;    
+        }  
         return dummy.next;
     }
 }
