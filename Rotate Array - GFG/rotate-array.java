9641076d52bea540056381ff9021e71c0424afb8
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2022-12-09 12:22:10 +0530

    Added solution - LeetHub

---
+++ Rotate Array - GFG/rotate-array.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out=new PrintWriter(System.out);
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		String[] starr=str.split(" ");
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(starr[0]);
+    		int d= Integer.parseInt(starr[1]);
+    		
+            int[] arr=new int[n];
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		  arr[j]= Integer.parseInt(starr1[j]);
+    		}
+    		
+    		//calling rotateArr() function
+            new Solution().rotateArr(arr, d, n);
+    		StringBuffer sb=new StringBuffer();
+    		
+    		//printing the elements of the array
+            for(int t1=0;t1<n;t1++)
+                sb.append(arr[t1]+" ");
+            out.println(sb);
+         }
+         out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    static void rotateArr(int nums[], int d, int n)
+    {
+        /*
+        for eg: arr = [1,2,3,4,5] k=2, 
+        Now reverse the array. It becomes [5,4,3,2,1]
+        Now split into two parts based on k value. like [5,4,3] and [2,1]
+        Now reverse the both parts.
+*/
+        d =  d%nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,n-d-1);
+        reverse(nums,n-d,nums.length-1);
+    }
+    public static void reverse(int []arr,int start,int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
