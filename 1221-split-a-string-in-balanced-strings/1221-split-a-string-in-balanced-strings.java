d99996b741b2dd33aba4f85b1487b14e1d4c4033
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2022-12-10 10:32:09 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (85.41%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,20 +1,6 @@
 class Solution {
     public int balancedStringSplit(String s) {
         /*
-        int ans =0;
-        int count =1;
-        char tar;
-         if(s.charAt(0)=='L') tar = 'R';
-         else tar ='L';
-        for(int i=1;i<s.length();i++){
-            if(s.charAt(i)==tar){
-                count--;
-                if(count==0) ans++;
-            }
-            else count++;
-        }
-        return ans;
-        */
         int ans=0;
         Stack<Character> st = new Stack<>();
         st.push(s.charAt(0));
@@ -26,6 +12,15 @@ class Solution {
             else{
                 st.push(s.charAt(i));
             }
+        }
+        return ans;
+        */
+        int count = 0;
+        int ans = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'L') count++;
+            else count--;
+            if(count == 0) ans++;
         }
         return ans;
     }
