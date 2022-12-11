15614db7215419ff649b126f8e180dc2f49bc85c
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2022-12-11 20:14:30 +0530

    Time: 35 ms (21.04%), Space: 57.6 MB (13.06%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int d=0,m=0;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;++i){
+            if(set.contains(nums[i])) d=nums[i];
+            set.add(nums[i]);
+        }
+        for(int i=1; i<=nums.length;++i){
+            if(set.contains(i)==false){
+                m = i;
+                break;
+            }
+        }
+        return new int[]{d,m};
+    }
+}
