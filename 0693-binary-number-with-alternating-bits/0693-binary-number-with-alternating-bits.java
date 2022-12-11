f7688c355f734bc8b31f0e5e52d8951e713d4ec4
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2022-12-11 17:02:32 +0530

    Update 0693-binary-number-with-alternating-bits.java

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.java
@@ -12,5 +12,16 @@ class Solution {
            }
        }
         return true;
+        /*
+        We know that if we shift the number by 1 to the right, all the ones will become zeros and vice versa. 
+        Now if we AND those two numbers, we can get the whole thing as zero but that won't work for numbers like 2, 4, 8... 
+        So we will take another approach. Instead of AND we will do an XOR .
+        This will make all bits 1. Now we need to check if all the bits are 1.
+        The best way to do that is AND the number by (number+1) . It'll give you zero.
+        */
+        /*
+        n = n ^ (n>>1);
+        return (n & n+1) == 0;
+        */
     }
 }
