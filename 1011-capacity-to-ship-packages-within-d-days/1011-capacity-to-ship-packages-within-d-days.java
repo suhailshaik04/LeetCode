cde295864fea8f59b26858add294d6bbf2278c30
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2023-02-22 15:02:22 +0530

    Time: 6 ms (99.46%), Space: 54.1 MB (9.51%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,22 @@
+class Solution {
+     public int shipWithinDays(int[] a, int days) {
+        int l = 0, r = 500 * a.length / days;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (isEnough(a, m, days)) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+    private boolean isEnough(int[] a, int m, int d) {
+        int cnt = 1, sum = 0;
+        for (int w : a) {
+            if (w > m) return false;
+            if ((sum += w) > m) {
+                if (++cnt > d) return false;
+                sum = w;
+            }
+        }
+        return true;
+    }
+}
