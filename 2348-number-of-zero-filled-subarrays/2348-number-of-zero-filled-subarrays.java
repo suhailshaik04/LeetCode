d61ac91072264fadb400bd6653c151e09b80ceb2
Shaik Mohammad Suhail <98518088+suhailshaik04@users.noreply.github.com>
2023-03-22 10:54:52 +0530

    Time: 3 ms (99.69%), Space: 60 MB (42.01%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = 0;
+            while (i < nums.length && nums[i] == 0) {
+                cur += 1;
+                res += cur;
+                i++;
+            }
+        }
+        return res;
+    }
+}
